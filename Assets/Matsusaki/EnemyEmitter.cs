034922fe363bce88b315ad424760ec0ac2672792
YamatoMatuki <dmat1224kamimamita@ezweb.ne.jp>
2017-10-19 18:09:07 +0900

    エみったー

--- Assets/Matsusaki/EnemyEmitter.cs
+++ Assets/Matsusaki/EnemyEmitter.cs
@@ -7,9 +7,11 @@ public class EnemyEmitter : MonoBehaviour
 
   //  public float minLength = 5f;
     //public float maxLength = 15f;
-    public float maxEnemy = 10;
+    public float maxEnemy1 = 10;
+    public float maxEnemy2 = 3;
 
-    public Enemy enemy;
+    public Enemy enemy1;
+    public Enemy enemy2;
 
     void Start()
     {
@@ -32,7 +34,7 @@ public class EnemyEmitter : MonoBehaviour
     void EmitEnemies()
     {
 
-        for (int i = 0; i < maxEnemy; i++)
+        for (int i = 0; i < maxEnemy1; i++)
         {
            // Vector3 rv = Random.onUnitSphere;
 
@@ -42,8 +44,16 @@ public class EnemyEmitter : MonoBehaviour
             float x = Random.Range(-25f,25f);
             float y = Random.Range(5f, 7f);
             float z = Random.Range(-25f, 25f);
-            Vector3 po = new Vector3(x, y, z);
-            Instantiate(enemy, po, Quaternion.identity);
+            Vector3 po1 = new Vector3(x, y, z);
+            Instantiate(enemy1, po1, Quaternion.identity);
+        }
+        for (int b = 0; b < maxEnemy2; b++) {
+            float v = Random.Range(-25f, 25f);
+            float w = Random.Range(-25f, 25f);
+            Vector3 po2 = new Vector3(v, 0, w);
+            Instantiate(enemy2, po2, Quaternion.identity);
+
+
         }
 
     }
