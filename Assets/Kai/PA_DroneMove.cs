e850dd74de427de15d2a91ef593e640214da901f
yuto1234567890 <k.yuto_199688@docomo.ne.jp>
2017-10-24 14:31:46 +0900

    yutoテスト

--- Assets/Kai/PA_DroneMove.cs
+++ Assets/Kai/PA_DroneMove.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PA_DroneMove : MonoBehaviour {
+
+	public Transform target;
+
+	private Animator animator;
+
 	void Update(){
 		transform.position = new Vector3 (transform.position.x, Mathf.PingPong (Time.time,0.03f)+2, transform.position.z);
 	}
-    private Animator animator;
 
 
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
+
+		transform.LookAt (target);
+
     }
 
 }
