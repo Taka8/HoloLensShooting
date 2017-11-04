145f8fc7112197a20a986a73cb06d11a0906cf20
YamatoMatuki <dmat1224kamimamita@ezweb.ne.jp>
2017-11-04 13:14:41 +0900

    iseri

--- Assets/Load.cs
+++ Assets/Load.cs
@@ -2,26 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Load : MonoBehaviour {
-    GameObject button;
-    private float time = 60;
+       
     public void SceneLoad()
     {
         SceneManager.LoadScene("shooting");
     }
     // Use this for initialization
     void Start () {
-        button = GameObject.Find("Canvas/Button");
-        button.SetActive(false);
-        time -= Time.deltaTime;
+            
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (time == 0) {
-            button.SetActive(true);
-        }
+        
 	}
 }
