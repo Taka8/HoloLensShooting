cf93e0a96222003340b972fc2e109f3ebe4cab94
YamatoMatuki <dmat1224kamimamita@ezweb.ne.jp>
2017-11-04 18:04:29 +0900

    Revert "井芹作用５"
    This reverts commit 4edfcdebc4ce5891e5f37e4726853ad644e06a50.

--- Assets/Kai/PA_DroneMove.cs
+++ Assets/Kai/PA_DroneMove.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PA_DroneMove : MonoBehaviour {
-	private Vector3 moveDirection=Vector3.zero;
 	void Update(){
         //transform.position = new Vector3 (transform.position.x, Mathf.PingPong (Time.time,0.03f)+2, transform.position.z);
-		//transform.position = new Vector3 (transform.position.x,transform.position.y, transform.position.z);
-		moveDirection.y= Mathf.PingPong (Time.time,0.03f);
+        transform.position = new Vector3(transform.position.x,transform.position.y, transform.position.z);
+        
     }
     private Animator animator;
 
